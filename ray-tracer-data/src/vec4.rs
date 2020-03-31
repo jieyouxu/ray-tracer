0679bb08e835bc27cd79935f52e498c48ffbfd4d
许杰友 Jieyou Xu (Joe) <jieyouxu@outlook.com>
2020-03-31 22:59:15 +0800

    test(vec3): add tests

--- ray-tracer-data/src/vec4.rs
+++ ray-tracer-data/src/vec4.rs
@@ -30,3 +30,53 @@ impl<N> From<(N, N, N, N)> for Vec4<N> {
         Self(t.0, t.1, t.2, t.3)
     }
 }
+
+impl<N: Copy> From<&(N, N, N, N)> for Vec4<N> {
+    fn from(t: &(N, N, N, N)) -> Self {
+        Self(t.0, t.1, t.2, t.3)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_new_and_getters() {
+        let vec4 = Vec4::new(1, 2, 3, 4);
+        assert_eq!(1, *vec4._0());
+        assert_eq!(2, *vec4._1());
+        assert_eq!(3, *vec4._2());
+        assert_eq!(4, *vec4._3());
+    }
+
+    #[test]
+    fn test_from_4_tuple_by_value() {
+        let t = (1, 2, 3, 4);
+        let vec4 = Vec4::from(t);
+        assert_eq!(1, *vec4._0());
+        assert_eq!(2, *vec4._1());
+        assert_eq!(3, *vec4._2());
+        assert_eq!(4, *vec4._3());
+    }
+
+    #[test]
+    fn test_from_4_tuple_by_ref() {
+        let t = (1, 2, 3, 4);
+        let vec4 = Vec4::from(&t);
+        assert_eq!(1, *vec4._0());
+        assert_eq!(2, *vec4._1());
+        assert_eq!(3, *vec4._2());
+        assert_eq!(4, *vec4._3());
+    }
+
+    #[test]
+    fn test_map() {
+        let vec4 = Vec4::new(1, 2, 3, 4);
+        let inc_vec4 = vec4.map(|x| x + 1);
+        assert_eq!(2, *inc_vec4._0());
+        assert_eq!(3, *inc_vec4._1());
+        assert_eq!(4, *inc_vec4._2());
+        assert_eq!(5, *inc_vec4._3());
+    }
+}
