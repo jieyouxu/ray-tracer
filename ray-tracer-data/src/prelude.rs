5a193c0ad675e8b8142bcad0e8ab5e953e89497a
许杰友 Jieyou Xu (Joe) <jieyouxu@outlook.com>
2020-03-31 22:36:06 +0800

    feat(rgb): add RGB data type

---
+++ ray-tracer-data/src/prelude.rs
@@ -0,0 +1,11 @@
+//! Prelude. Includes useful data types that should usually be imported when trying to implement a
+//! ray tracer.
+//!
+//! ## Example Usage
+//!
+//! ```rust,no_run
+//! use ray_tracer_data::prelude::*;
+//! ```
+
+pub use crate::rgb::*;
+pub use crate::vec3::*;
