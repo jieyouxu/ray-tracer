c539e644ad4837714e8e02f6e865bc3ef0bacce9
许杰友 Jieyou Xu (Joe) <39484203+jieyouxu@users.noreply.github.com>
2020-04-01 00:29:55 +0800

    feat(ray-tracer-data): add initial `ray-tracer-data` (#2)
    * infra: setup
    
    * feat(vec3): add 3-component vector
    
    * feat(vec4): add 4-component vector
    
    * feat(vec3): impl by-ref getters and conversion from 3-tuple
    
    * feat(vec4): impl by-ref getters and conversion from 4-tuple
    
    * feat(rgb): add RGB data type
    
    * feat(prelude): add prelude
    
    * feat(const-fn): mark constructor and getters as `const-fn`s
    
    * test(vec3): add tests
    
    * feat(vec): add conversions from tuple-refs by copy
    
    * chore: export `vec4` module
    
    * feat(map): add `map` to `Vec3` and `Vec4`.
    
    * feat(prelude): add prelude
    
    * feat(rgb): add RGB data type

---
+++ ray-tracer-data/src/lib.rs
@@ -0,0 +1,5 @@
+//! Core domain data types for the ray tracer.
+
+pub mod rgb;
+pub mod vec3;
+pub mod vec4;
---
+++ ray-tracer-data/src/vec4.rs
@@ -0,0 +1,94 @@
+/// A 4-component vector.
+pub struct Vec4<N>(N, N, N, N);
+
+impl<N> Vec4<N> {
+    /// Constructs a new 4-component vector.
+    pub const fn new(_0: N, _1: N, _2: N, _3: N) -> Self {
+        Self(_0, _1, _2, _3)
+    }
+
+    /// Gets the first component of the vector.
+    pub const fn _0(&self) -> &N {
+        &self.0
+    }
+    /// Gets the second component of the vector.
+    pub const fn _1(&self) -> &N {
+        &self.1
+    }
+    /// Gets the third component of the vector.
+    pub const fn _2(&self) -> &N {
+        &self.2
+    }
+    /// Gets the fourth component of the vector.
+    pub const fn _3(&self) -> &N {
+        &self.3
+    }
+}
+
+impl<N> Vec4<N>
+where
+    N: Copy,
+{
+    pub fn map<T>(&self, f: fn(N) -> T) -> Vec4<T>
+    where
+        T: Copy,
+    {
+        Vec4(f(self.0), f(self.1), f(self.2), f(self.3))
+    }
+}
+
+impl<N> From<(N, N, N, N)> for Vec4<N> {
+    fn from(t: (N, N, N, N)) -> Self {
+        Self(t.0, t.1, t.2, t.3)
+    }
+}
+
+impl<N: Copy> From<&(N, N, N, N)> for Vec4<N> {
+    fn from(t: &(N, N, N, N)) -> Self {
+        Self(t.0, t.1, t.2, t.3)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_new_and_getters() {
+        let vec4 = Vec4::new(1, 2, 3, 4);
+        assert_eq!(1, *vec4._0());
+        assert_eq!(2, *vec4._1());
+        assert_eq!(3, *vec4._2());
+        assert_eq!(4, *vec4._3());
+    }
+
+    #[test]
+    fn test_from_4_tuple_by_value() {
+        let t = (1, 2, 3, 4);
+        let vec4 = Vec4::from(t);
+        assert_eq!(1, *vec4._0());
+        assert_eq!(2, *vec4._1());
+        assert_eq!(3, *vec4._2());
+        assert_eq!(4, *vec4._3());
+    }
+
+    #[test]
+    fn test_from_4_tuple_by_ref() {
+        let t = (1, 2, 3, 4);
+        let vec4 = Vec4::from(&t);
+        assert_eq!(1, *vec4._0());
+        assert_eq!(2, *vec4._1());
+        assert_eq!(3, *vec4._2());
+        assert_eq!(4, *vec4._3());
+    }
+
+    #[test]
+    fn test_map() {
+        let vec4 = Vec4::new(1, 2, 3, 4);
+        let inc_vec4 = vec4.map(|x| x + 1);
+        assert_eq!(2, *inc_vec4._0());
+        assert_eq!(3, *inc_vec4._1());
+        assert_eq!(4, *inc_vec4._2());
+        assert_eq!(5, *inc_vec4._3());
+    }
+}
